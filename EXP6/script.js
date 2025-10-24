db8c48f76b9e31bad3b25b1c0514216eb7745da4
RafiunnisaMohammad <151987179+RafiunnisaMohammad@users.noreply.github.com>
2025-10-24 12:31:34 +0530

    Add files via upload

---
+++ EXP6/script.js
@@ -0,0 +1,5 @@
+function externalMsg() {
+  const msg = document.getElementById("msg");
+  msg.innerHTML = "🚀 Hello from External JavaScript (via script.js)!";
+  msg.style.color = "#4caf50"; // Green for external JS
+}
